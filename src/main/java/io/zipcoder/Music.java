13b465674e945f7399fc07f4dcbca48f413251c9
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-23 14:48:12 -0500

    lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -13,39 +13,53 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection) {
-        // need to return Integer
-        // result will be the MIN number of presses needed to reach song SELECTION
-        // if else statements
-        List<String> list = new ArrayList<>(Arrays.asList(playList));
-        int forward = 0;
-        int backward = 0;
-        int buttonPresses = 0;
+//        // need to return Integer
+//        // result will be the MIN number of presses needed to reach song SELECTION
+//        // if else statements
+//        List<String> list = new ArrayList<>(Arrays.asList(playList));
+//        int forward = 0;
+//        int backward = 0;
+//        int buttonPresses = 0;
+//
+//        for (int i = startIndex; i <= list.size() - 1; i++) {
+//            if (!selection.equals(list.get(i))) {
+//                forward++;
+//            }
+//        }
+//
+//        for (int j = startIndex; j <= list.size() - 1; j--) {
+//            backward++;
+//            if (selection.equals(list.get(list.size() - 1 - j))) {
+//                break;
+//            }
+//        }
+//
+//        if (forward < backward) {
+//            buttonPresses = forward;
+//        }
+//        if (backward == forward) {
+//            buttonPresses = forward;
+//        }
+//        if (backward < forward) {
+//            buttonPresses = backward;
+//        }
+//
+//        return buttonPresses;
 
-        for (int i = startIndex; i <= list.size() - 1; i++) {
-            if (!selection.equals(list.get(i))) {
-                forward++;
+
+        List<String> stringMatch = new ArrayList<String>(Arrays.asList(playList));
+        startIndex = 1;
+        for (int i = startIndex; i <= stringMatch.size(); i++) {
+            if (stringMatch.contains(selection)) {
+
             }
         }
-
-        for (int j = startIndex; j <= list.size() - 1; j--) {
-            backward++;
-            if (selection.equals(list.get(list.size() - 1 - j))) {
-                break;
-            }
-        }
-
-        if (forward < backward) {
-            buttonPresses = forward;
-        }
-        if (backward == forward) {
-            buttonPresses = forward;
-        }
-        if (backward < forward) {
-            buttonPresses = backward;
-        }
-
-        return buttonPresses;
+        System.out.println(stringMatch.indexOf(selection) - startIndex);
+        return stringMatch.indexOf(selection) - startIndex;
     }
+
+
+
 
     private Object[] getPlayList() {
         return playList;
